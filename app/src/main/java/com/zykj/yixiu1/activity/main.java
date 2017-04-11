229d55caf9e78faded3e0a84f1636d41cd14b081
zz199204 <563700780@qq.com>
2017-04-11 11:45:09 +0800

    Initial commit

---
+++ app/src/main/java/com/zykj/yixiu1/activity/main.java
@@ -0,0 +1,18 @@
+package com.zykj.yixiu1.activity;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+import com.zykj.yixiu1.R;
+
+/**
+ * Created by zykj on 2017/4/11.
+ */
+
+public class main extends Activity {
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
