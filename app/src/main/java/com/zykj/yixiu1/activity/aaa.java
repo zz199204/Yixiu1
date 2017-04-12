37d195c81630bb6e53230956652563d0a02b301d
zz199204 <563700780@qq.com>
2017-04-12 23:48:42 +0800

    jiage  aaaa

---
+++ app/src/main/java/com/zykj/yixiu1/activity/aaa.java
@@ -0,0 +1,16 @@
+package com.zykj.yixiu1.activity;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+/**
+ * Created by Administrator on 2017/4/12.
+ */
+
+public class aaa extends Activity {
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(android.R.layout.activity_list_item);
+    }
+}
